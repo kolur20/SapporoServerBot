b49cb5262cfbf42f9059b141273320389ba08268
Kirill <Kirill>
2019-03-05 00:41:48 +0300

    Добавьте файлы проекта.

---
+++ ServerTelegramBot/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerTelegramBot
+{
+    static class MainClass
+    {
+        static NotifyIcon notify = new NotifyIcon();
+        static ContextMenuStrip contextMenu = new ContextMenuStrip();
+        static void Main()
+        {
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Перезапуск", Properties.Resource.reload, MainClass_ClickReload).Name = "Reload";
+            contextMenu.Items.Add("Закрыть", Properties.Resource.close, MainClass_ClickClose).Name = "Close";
+            
+            
+
+
+            NotifyIcon notify = new NotifyIcon();
+            notify.Icon = Properties.Resource.media;
+            notify.Visible = true;
+            notify.Text = "Server BotTelegram";
+            notify.ContextMenuStrip = contextMenu;
+
+            
+            Application.Run();
+        }
+
+        private static void MainClass_ClickReload(object sender, EventArgs e)
+        {
+          
+        }
+
+        private static void MainClass_ClickClose(object sender, EventArgs e)
+        {
+            notify.Dispose();
+            Application.Exit();
+
+        }
+    }
+}
---
+++ ServerTelegramBot/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("Server HelperSapporo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServerTelegramBot.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ff407b0f-514f-4bcd-9455-7190c3194a5f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
